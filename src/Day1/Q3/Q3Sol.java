ab0ecdd3cee5c015e731d49e4f03509b433418d0
LeShaurya <raoljishauryadeep@gmail.com>
2025-01-07 16:34:26 +0530

    Day-1 Q3

--- src/Day1/Q3/Q3Sol.java
+++ src/Day1/Q3/Q3Sol.java
@@ -1,4 +1,18 @@
 package Day1.Q3;
 
 public class Q3Sol {
+    public static int[] copyOf(int[] array) {
+        int[] copy = new int[array.length];
+//        System.arraycopy(array, 0, copy, 0, array.length);
+        for(int i = 0; i < array.length; i++)
+            copy[i] = array[i];
+        return copy;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int[] copy = copyOf(arr);
+        for(int i : copy)
+            System.out.println(i);
+    }
 }
