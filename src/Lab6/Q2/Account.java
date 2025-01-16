c112399c5604f681000eda170f0d65cc64c65b40
LeShaurya <raoljishauryadeep@gmail.com>
2025-01-16 09:39:03 +0530

    Lab 6 Q2 comments for issues

--- src/Lab6/Q2/Account.java
+++ src/Lab6/Q2/Account.java
@@ -14,6 +14,9 @@ public class Account {
         this.balance = balance;
     }
 
+/**
+    * Why does this piece of code give different output despite using the key {@code synchronized}.
+ **/
     public synchronized void addAmount(double amount) {
         double temp = this.balance;
         try {
@@ -21,12 +24,15 @@ public class Account {
         } catch (InterruptedException e) {
             System.out.println("Operation interrupted.");
         }
-
         temp += amount;
         this.balance += temp;
         System.out.println("Deposited amount of: " + amount);
     }
 
+    /**
+     * {@code temp} variable is not required but still is needed to counter race condition
+     * despite using {@code synchronized}.
+     */
     public synchronized void subtractAmount(double amount) {
         double temp = this.balance;
         try {
@@ -34,7 +40,6 @@ public class Account {
         } catch (InterruptedException e) {
             System.out.println("Operation interrupted.");
         }
-
         if(this.balance >= amount) {
             temp -= amount;
             this.balance -= temp;
